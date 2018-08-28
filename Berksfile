4489980e2acc9c7a3541e99cf6ca7163474de00b
Cailet Latham <clatham@Cailets-MacBook-Pro.local>
2018-08-28 17:34:11 +0100

    Added a bunch more benchmarks

--- Berksfile
+++ Berksfile
@@ -3,5 +3,4 @@ source 'https://supermarket.chef.io'
 metadata
 
 cookbook 'apt', '~> 6.1.4'
-cookbook 'line', '2.0.0'
-
+cookbook 'line', '2.0.2'
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,6 +2,8 @@
 # These scripts are intentionally inefficient to make it easier to break each
 # "feature" down to a specific spec in the benchmarks
 
+include_recipe 'apt'
+
 # Benchmarks: 1.1.1.1 - 1.1.1.6
 file '/etc/modprobe.d/disabled_filesystems.conf' do
   owner 'root'
@@ -43,8 +45,11 @@ service 'autofs' do
   action :disable
 end
 
-# Benchmark: 1.3
-package 'aide'
+# Benchmark: 1.3.1
+package 'aide' do
+  action :install
+#  notifies :run 'execute[aideinit]' TODO: finish implementation of aideinit
+end
 
 # TODO: Finish implementation of 1.3.2
 
@@ -92,6 +97,11 @@ package 'prelink' do
   action :remove
 end
 
+# Benchmark 1.6.3
+package 'apparmor' do
+  action :upgrade
+end
+
 # Benchmark: 1.7.1.1
 %w(10-help-text 51-cloudguest 90-updates-available 91-release-upgrade).each do |motd|
   file "/etc/update-motd.d/#{motd}" do
@@ -131,6 +141,125 @@ package 'openbsd-inetd' do
 end
 
 # Benchmark: 2.2.1.1
-package 'chrony'
+package 'chrony' do
+  action :install
+end
 
+# Benchmark: 2.2.2
+package 'xserver-xorg' do
+  action :remove
+end
 
+# Benchmark: 2.2.3 - 2.2.17
+%w(
+  avahi-daemon
+  cups
+  isc-dhcp-server
+  isc-dhcp-server6
+  slapd
+  nfs-server
+  bind9
+  vsftpd
+  apache2
+  dovecot
+  smbd
+  squid
+  snmpd
+  postfix
+  rsync
+  nis
+).each do |s|
+  service s do
+    action :disable
+  end
+end
+
+systemd_unit 'rpcbind' do
+  action :mask
+end
+
+# Benchmark: 2.3.1 - 2.3.5
+%w(
+  nis
+  rsh-client
+  rsh-redone-client
+  talk
+  telnet
+  ldap-utils
+).each do |a|
+  package a do
+    action :remove
+  end
+end
+
+# Benchmark: 3.1.2 - 3.2.8
+execute 'flush_routes' do
+  command 'sysctl -w net.ipv4.route.flush=1'
+  action :nothing
+end
+
+delete_lines 'remove_comments_from_/etc/sysctl.conf' do
+  path '/etc/sysctl.conf'
+  pattern /^#.*/
+end
+
+delete_lines 'remove_empty_lines' do
+  path '/etc/sysctl.conf'
+  pattern /^\s*$/
+end
+
+[
+  "net.ipv4.conf.all.send_redirects",
+  "net.ipv4.conf.default.send_redirects",
+  "net.ipv4.conf.all.accept_source_route",
+  "net.ipv4.conf.default.accept_source_route",
+  "net.ipv4.conf.all.accept_redirects",
+  "net.ipv4.conf.default.accept_redirects",
+  "net.ipv4.conf.all.secure_redirects",
+  "net.ipv4.conf.default.secure_redirects"
+].each do |line|
+  append_if_no_line "add_#{line}_to_/etc/sysctl.conf" do
+    path '/etc/sysctl.conf'
+    line "#{line} = 0"
+    notifies :run, "execute[apply_#{line}]", :immediately
+  end
+  execute "apply_#{line}" do
+    command "sysctl -w #{line}=0"
+    action :nothing
+    notifies :run, 'execute[flush_routes]'
+  end
+end
+
+[
+  "net.ipv4.conf.all.log_martians",
+  "net.ipv4.conf.default.log_martians",
+  "net.ipv4.icmp_echo_ignore_broadcasts",
+  "net.ipv4.icmp_ignore_bogus_error_responses",
+  "net.ipv4.conf.all.rp_filter",
+  "net.ipv4.conf.default.rp_filter",
+  "net.ipv4.tcp_syncookies"
+].each do |line|
+  append_if_no_line "add_#{line}_to_/etc/sysctl.conf" do
+    path '/etc/sysctl.conf'
+    line "#{line} = 1"
+    notifies :run, "execute[apply_#{line}]", :immediately
+  end
+  execute "apply_#{line}" do
+    command "sysctl -w #{line}=1"
+    action :nothing
+    notifies :run, 'execute[flush_routes]'
+  end
+end
+
+# Benchmark 3.5.1 - 3.5.4
+%w(dccp sctp rds tipc).each do |f|
+  append_if_no_line "disable_#{f}_protocol" do
+    path '/etc/modprobe.d/disabled_protocols.conf'
+    line "install #{f} /bin/true"
+  end
+end
+
+# Benchmark 3.6.1
+package 'iptables' do
+  action :upgrade
+end
