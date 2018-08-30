298e075242b84ec93887c1739e2077f303f3464d
Chris Pressland <cpressland@bink.com>
2018-08-30 15:56:28 +0000

    Collapse Benchmarks 4.1.4 to 4.1.18 into a template block

--- recipes/default.rb
+++ recipes/default.rb
@@ -313,87 +313,16 @@ execute 'update-grub' do
   action :nothing
 end
 
-# Benchmark 4.1.4 - 4.1.11
-file '/etc/audit/audit.rules' do
+# Benchmark 4.1.4 - 4.1.18
+template '/etc/audit/audit.rules' do
   owner 'root'
   group 'root'
   mode 0644
-  action :create_if_missing
-end
-
-replace_or_add 'set_auditd_buffer_size' do
-  path '/etc/audit/audit.rules'
-  pattern '-b 320'
-  line '-b 8192'
-end
-
-[
-  "-f 1",
-  "-i",
-  "-a always,exit -F arch=b64 -S adjtimex -S settimeofday -k time-change",
-  "-a always,exit -F arch=b32 -S adjtimex -S settimeofday -S stime -k time-change",
-  "-a always,exit -F arch=b64 -S clock_settime -k time-change",
-  "-a always,exit -F arch=b32 -S clock_settime -k time-change",
-  "-w /etc/localtime -p wa -k time-change",
-  "-w /etc/group -p wa -k identity",
-  "-w /etc/passwd -p wa -k identity",
-  "-w /etc/gshadow -p wa -k identity",
-  "-w /etc/shadow -p wa -k identity",
-  "-w /etc/security/opasswd -p wa -k identity",
-  "-a always,exit -F arch=b64 -S sethostname -S setdomainname -k system-locale",
-  "-a always,exit -F arch=b32 -S sethostname -S setdomainname -k system-locale",
-  "-w /etc/issue -p wa -k system-locale",
-  "-w /etc/issue.net -p wa -k system-locale",
-  "-w /etc/hosts -p wa -k system-locale",
-  "-w /etc/apparmor/ -p wa -k MAC-policy",
-  "-w /etc/apparmor.d/ -p wa -k MAC-policy",
-  "-w /var/log/faillog -p wa -k logins",
-  "-w /var/log/lastlog -p wa -k logins",
-  "-w /var/log/tallylog -p wa -k logins",
-  "-w /var/run/utmp -p wa -k session",
-  "-w /var/log/wtmp -p wa -k logins",
-  "-w /var/log/btmp -p wa -k logins",
-  "-a always,exit -F arch=b64 -S chmod -S fchmod -S fchmodat -F auid>=1000 -F auid!=4294967295 -k perm_mod",
-  "-a always,exit -F arch=b32 -S chmod -S fchmod -S fchmodat -F auid>=1000 -F auid!=4294967295 -k perm_mod",
-  "-a always,exit -F arch=b64 -S chown -S fchown -S fchownat -S lchown -F auid>=1000 -F auid!=4294967295 -k perm_mod",
-  "-a always,exit -F arch=b32 -S chown -S fchown -S fchownat -S lchown -F auid>=1000 -F auid!=4294967295 -k perm_mod",
-  "-a always,exit -F arch=b64 -S setxattr -S lsetxattr -S fsetxattr -S removexattr -S lremovexattr -S fremovexattr -F auid>=1000 -F auid!=4294967295 -k perm_mod",
-  "-a always,exit -F arch=b32 -S setxattr -S lsetxattr -S fsetxattr -S removexattr -S lremovexattr -S fremovexattr -F auid>=1000 -F auid!=4294967295 -k perm_mod",
-  "-a always,exit -F arch=b64 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EACCES -F auid>=1000 -F auid!=4294967295 -k access",
-  "-a always,exit -F arch=b32 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EACCES -F auid>=1000 -F auid!=4294967295 -k access",
-  "-a always,exit -F arch=b64 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EPERM -F auid>=1000 -F auid!=4294967295 -k access",
-  "-a always,exit -F arch=b32 -S creat -S open -S openat -S truncate -S ftruncate -F exit=-EPERM -F auid>=1000 -F auid!=4294967295 -k access"
-].each do |line|
-  append_if_no_line "add_#{line}_to_/etc/audit/audit.rules" do
-    path '/etc/audit/audit.rules'
-    line "#{line}"
-    notifies :restart, 'service[auditd]'
-  end
+  source 'auditd/rules.erb'
+  notifies :restart, 'service[auditd]'
 end
 
 # TODO: Research Benchmark 4.1.12 & update bookstack
-
-# Benchmark 4.1.13 - 4.1.18
-[
-  "-a always,exit -F arch=b64 -S mount -F auid>=1000 -F auid!=4294967295 -k mounts",
-  "-a always,exit -F arch=b32 -S mount -F auid>=1000 -F auid!=4294967295 -k mounts",
-  "-a always,exit -F arch=b64 -S unlink -S unlinkat -S rename -S renameat -F auid>=1000 -F auid!=4294967295 -k delete",
-  "-a always,exit -F arch=b32 -S unlink -S unlinkat -S rename -S renameat -F auid>=1000 -F auid!=4294967295 -k delete",
-  "-w /etc/sudoers -p wa -k scope",
-  "-w /etc/sudoers.d/ -p wa -k scope",
-  "-w /var/log/sudo.log -p wa -k actions",
-  "-w /sbin/insmod -p x -k modules",
-  "-w /sbin/rmmod -p x -k modules",
-  "-w /sbin/modprobe -p x -k modules",
-  "-a always,exit -F arch=b64 -S init_module -S delete_module -k modules",
-  "-e 2"
-].each do |line|
-  append_if_no_line "add_#{line}_to_/etc/audit/audit.rules" do
-    path '/etc/audit/audit.rules'
-    line "#{line}"
-    notifies :restart, 'service[auditd]'
-  end
-end
 
 # Benchmark 4.2.1.1
 service 'rsyslog' do
