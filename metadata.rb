5bf12ff66d70fc1f68b9e4f33c45344dcd4e8649
Terry Cain <tcain@bink.com>
2020-07-09 17:22:41 +0000

    Added inspec run script and systemd timer

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name 'nebula'
 description 'Implementation of CIS Benchmarks for Ubuntu'
-version '2.0.0'
+version '2.0.1'
 
 depends 'line'
 depends 'systemd'
--- recipes/inspec.rb
+++ recipes/inspec.rb
@@ -1,5 +1,9 @@
 directory '/usr/local/src/inspec'
 
+package 'python3-requests' do
+  action :install
+end
+
 remote_file '/usr/local/src/inspec/inspec.deb' do
   source node['inspec']['deb_url']
   checksum node['inspec']['deb_sha256']
@@ -19,4 +23,42 @@ remote_directory '/usr/local/src/inspec/profiles/base' do
   files_mode '0755'
   action :create
   recursive true
+end
+
+cookbook_file '/usr/local/src/inspec/inspec.py' do
+  source 'inspec.py'
+  owner 'root'
+  group 'root'
+  mode '0770'
+  action :create
+end
+
+systemd_unit 'inspec.service' do
+  content(
+    Unit: {
+      Description: 'Runs inspec and submits data to elasticsearch',
+      After: 'network-online.target',
+      Wants: 'network-online.target',
+    },
+    Service: {
+      Type: 'oneshot',
+      ExecStart: '/usr/local/src/inspec/inspec.py',
+    }
+  )
+  action [:create, :enable]
+end
+
+systemd_unit 'inspec.timer' do
+  content(
+    Unit: {
+      Description: 'Runs inspec and submits data to elasticsearch',
+    },
+    Timer: {
+      OnCalendar: '*-*-* 4:00:00',
+    },
+    Install: {
+      WantedBy: 'timers.target',
+    }
+  )
+  action [:create, :start, :enable]
 end
